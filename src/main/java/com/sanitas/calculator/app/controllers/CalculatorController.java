b978ec23952520409af1adf35137c7e6d42be9a4
Víctor José Gutiérrez Sáez <victorvelez87@gmail.com>
2023-03-26 17:40:00 +0200

    Agregada api externa tracer y cambio en pom.xml para cargarla

--- src/main/java/com/sanitas/calculator/app/controllers/CalculatorController.java
+++ src/main/java/com/sanitas/calculator/app/controllers/CalculatorController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sanitas.calculator.app.services.IOperacionesService;
 
+import io.corp.calculator.TracerImpl;
+
 /**
  * Controller que recibe las peticiones de 
  * cada una de las distintas operaciones aritméticas
@@ -29,6 +31,9 @@ public class CalculatorController {
 		
 		Double result = operacionesService.sumar(operando1, operando2);		
 		
+		TracerImpl tracer = new TracerImpl();
+		tracer.trace(result);
+		
 		return ResponseEntity.status(HttpStatus.OK).body(result.toString());
 	}
 	
@@ -38,6 +43,8 @@ public class CalculatorController {
 		
 		Double result = operacionesService.restar(minuendo, sustraendo);
 		
+		TracerImpl tracer = new TracerImpl();
+		tracer.trace(result);
 		
 		return ResponseEntity.status(HttpStatus.OK).body(result.toString());
 	}
