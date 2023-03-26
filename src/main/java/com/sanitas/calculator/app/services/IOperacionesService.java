fd92a76c853e1c1dcbb2a7e0d43edc76856d6b6c
Víctor José Gutiérrez Sáez <victorvelez87@gmail.com>
2023-03-26 16:17:25 +0200

    servicio de operaciones creado

---
+++ src/main/java/com/sanitas/calculator/app/services/IOperacionesService.java
@@ -0,0 +1,8 @@
+package com.sanitas.calculator.app.services;
+
+public interface IOperacionesService {
+	
+	public Double sumar(Double operador1, Double operador2);
+	
+	public Double restar(Double minuendo, Double sustraendo);
+}
---
+++ src/main/java/com/sanitas/calculator/app/services/OperacionesServiceImpl.java
@@ -0,0 +1,20 @@
+package com.sanitas.calculator.app.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OperacionesServiceImpl implements IOperacionesService {
+
+	@Override
+	public Double sumar(Double operador1, Double operador2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Double restar(Double minuendo, Double sustraendo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
